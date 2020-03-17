3b9d7428d29ca4db59ab86ebd28482a3c08490bd
Yasir-Mohamud <ymohamed018@gmail.com>
2020-03-17 12:53:07 -0700

    fixes server

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 'use strict';
-require('dotenv').config();
-const cors = require('cors');
 const express = require('express');
 const app = express();
+
+require('dotenv').config();
+
+const cors = require('cors');
+app.use(cors());
+
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
-app.use(express.static('./public'));
-app.get('./', (require,respond) => {
+app.get('/location', (request,response) => {
   try {
-    let city = require.query.city;
+    let city = request.query.city;
     let geo = require('./data/geo.json');
 
     let location = new Location (geo[0],city);
-    respond.send(location);
+    response.send(location);
   }
-  catch(err){console.error(err)}
+  catch(err) {
+    console.error(err)
+  }
 })
 
 function Location(obj, city) {
   this.search_query = city;
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
-  this.longitude = obj.lon
+  this.longitude = obj.lon;
 }
 
 
+app.use('*', (request,response) => response.send('Sorry , that route does not exist.'));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
-app.listen(PORT, () => console.log(`listenign on port ${PORT}`));
-app.use('*', (require,respond) => respond.send('Sorry , that route does not exist.'));
